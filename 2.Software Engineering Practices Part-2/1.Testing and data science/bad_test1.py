72127688e1da39113a61237f7498dd09728dc108
AnshuTrivedi <anshutrivedik@gmail.com>
2020-05-28 09:57:38 +0530

    add: bad test demo

---
+++ 2.Software Engineering Practices Part-2/1.Testing and data science/bad_test1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from nearest import nearest_square
+
+
+print("Nearest square <=5:",nearest_square(5))
+print("Nearest square <=-12:",nearest_square(-12))
+print("Nearest square <=13:",nearest_square(13))
+print("Nearest square <=23:",nearest_square(23))
